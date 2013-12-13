27e47edbb2754dd0ec50a2e2df286e48f154953a
Stéphane Erbrech <stephane.erbrech@gmail.com>
2013-12-13 13:06:30 +0100

    change path to views to be relative (issue #1)

--- NuGet/HotTowel-NG/app/services/directives.js
+++ NuGet/HotTowel-NG/app/services/directives.js
@@ -190,7 +190,7 @@
                 'rightText': '@',
                 'allowCollapse': '@'
             },
-            templateUrl: '/app/layout/widgetheader.html',
+            templateUrl: 'app/layout/widgetheader.html',
             restrict: 'A',
         };
         return directive;
