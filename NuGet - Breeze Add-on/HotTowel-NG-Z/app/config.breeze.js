d6a15ae59472e4b088e8c9cfe37d2af24d6c5dcb
John Papa <john@johnpapa.net>
2014-01-26 20:31:20 -0500

    HotTowel.Angular.Bootstrap now supporting Breeze 1.4.8 and angular.q changes

---
+++ NuGet - Breeze Add-on/HotTowel-NG-Z/app/config.breeze.js
@@ -0,0 +1,26 @@
+﻿// ReSharper disable InconsistentNaming
+(function () {
+    'use strict';
+
+    // app.js injects, triggering config
+    angular.module('app')
+           .factory('config.breeze', ['$q', '$http', 'use$q', configBreeze]);
+       
+    function configBreeze($q, $http, use$q) {
+        // use $q for promises
+        use$q($q);
+
+        // use the current module's $http for ajax calls
+        var ajax = breeze.config.initializeAdapterInstance('ajax', 'angular');
+        ajax.setHttp($http);
+
+        // backingStore works for Angular (ES5 property 'ready')
+        breeze.config.initializeAdapterInstance('modelLibrary', 'backingStore', true);
+
+        // Tell breeze not to validate when we attach a newly created entity to any manager.
+        // We could also set this per entityManager
+        new breeze.ValidationOptions({ validateOnAttach: false }).setAsDefault();
+
+        breeze.NamingConvention.camelCase.setAsDefault();
+    }
+})();
