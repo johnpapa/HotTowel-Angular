af5cb5694a9e5d1ca378700fb1ffbda4c737e112
John Papa <john@johnpapa.net>
2014-03-25 00:40:38 -0400

    Updated for Breeze changes explained here http://www.johnpapa.net/new-breeze-angular-service/

--- NuGet - Breeze Add-on/HotTowel-NG-Z/app/services/entityManagerFactory.js
+++ NuGet - Breeze Add-on/HotTowel-NG-Z/app/services/entityManagerFactory.js
@@ -2,11 +2,14 @@
     'use strict';
     
     var serviceId = 'entityManagerFactory';
-    angular.module('app').factory(serviceId, ['config', emFactory]);
+    angular.module('app').factory(serviceId, ['breeze', 'config', emFactory]);
 
-    function emFactory(config) {
-        breeze.config.initializeAdapterInstance('modelLibrary', 'backingStore', true);
+    function emFactory(breeze, config) {
+        // Convert server-side PascalCase to client-side camelCase property names
         breeze.NamingConvention.camelCase.setAsDefault();
+        // Do not validate when we attach a newly created entity to an EntityManager.
+        // We could also set this per entityManager
+        new breeze.ValidationOptions({ validateOnAttach: false }).setAsDefault();
         
         var serviceName = config.remoteServiceName;
         var metadataStore = new breeze.MetadataStore();
---
+++ NuGet - Breeze Add-on/HotTowel-NG-Z/scripts/breeze.to$q.shim.js
@@ -0,0 +1,72 @@
+﻿// Legacy. Support apps that followed old recommendation of adding to$q
+// to the end of Breeze methods that returned Q.js promises.
+// Althought harmless, we don't need or want it with this module.
+// But it may take time to remove to$q from an existing app.
+// Remove this shim when you have replaced all references to "to$q" with "then" in your app.
+
+(function (definition, window) {
+    if (window.breeze) {
+        definition(window.breeze);
+    } else if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
+        // CommonJS or Node
+        var b = require('breeze');
+        definition(b);
+    } else if (typeof define === "function" && define["amd"] && !window.breeze) {
+        // Requirejs / AMD 
+        define(['breeze'], definition);
+    } else {
+        throw new Error("Can't find breeze");
+    }
+}(function (breeze) {
+
+    // EntityManager
+    var proto = breeze.EntityManager.prototype;
+    var executeQuery = proto.executeQuery;
+
+    if (-1 < executeQuery.toString().indexOf('extendWith_to$q')) {
+        return; // already extended Breeze
+    }
+
+    proto.executeQuery =
+        function () {
+            var promise = executeQuery.apply(this, arguments);
+            return extendWith_to$q(promise);
+        };
+
+    var e_fetchMetadata = proto.fetchMetadata;
+    proto.fetchMetadata =
+        function () {
+            var promise = e_fetchMetadata.apply(this, arguments);
+            return extendWith_to$q(promise);
+        };
+
+    var fetchEntityByKey = proto.fetchEntityByKey;
+    proto.fetchEntityByKey =
+        function () {
+            var promise = fetchEntityByKey.apply(this, arguments);
+            return extendWith_to$q(promise);
+        };
+
+    var saveChanges = proto.saveChanges;
+    proto.saveChanges =
+        function () {
+            var promise = saveChanges.apply(this, arguments);
+            return extendWith_to$q(promise);
+        };
+
+    // MetadataStore
+    proto = breeze.MetadataStore.prototype;
+    var m_fetchMetadata = proto.fetchMetadata;
+    proto.fetchMetadata =
+        function () {
+            var promise = m_fetchMetadata.apply(this, arguments);
+            return extendWith_to$q(promise);
+        };
+
+    function extendWith_to$q(promise) {
+        promise.to$q = function (success, fail) {
+            return (success || fail) ? promise.then(success, fail) : promise;
+        };
+        return promise;
+    }
+}, this));
